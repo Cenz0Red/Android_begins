ccad5f3eefe9c1ff197a4d99095cd80257de2c9f
Cenz0Red <159074780+Cenz0Red@users.noreply.github.com>
2024-07-04 21:48:08 +0300

    Create ItemsActivity.kt

---
+++ ItemsActivity.kt
@@ -0,0 +1,30 @@
+package com.example.onemoreapp
+
+import android.media.RouteListingPreference.Item
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemsActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_items)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val itemsList: RecyclerView = findViewById(R.id.itemsList)
+        val items = arrayListOf<Item>()
+
+        items.add(Item(1, "", "", "", "", 100))
+    }
+}
